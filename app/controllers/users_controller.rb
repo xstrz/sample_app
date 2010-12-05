cc67c71b06c293ff6626ca62f98e8a95f2137916
xstrz <vadim.belovalov@gmail.com>
2010-12-06 00:11:14 +0500

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
   def new
     @title = "Sing Up"
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      @title = "Sing Up"
+      render 'new'
+    end
   end
 
 end
