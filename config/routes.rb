a2dec903111715c3068e26ff5de45bca9491e4cc
xstrz <vadim.belovalov@gmail.com>
2010-12-06 16:00:20 +0500

    Done with sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 SampleApp::Application.routes.draw do
   resources :users
-  #get :show, :id => @user.id
-  
-  #match '/', :to => 'pages#home'
+  resources :sessions,  :only => [:new, :create, :destroy]
 
+  match '/singup',  :to => 'users#new'
+  match '/singin',  :to => 'sessions#new'
+  match '/singout', :to => 'sessions#destroy'
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  match '/singup',  :to => 'users#new'
+  
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
