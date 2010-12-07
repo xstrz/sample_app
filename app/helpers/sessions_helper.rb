5d1f8ba7596e37a4f632a70d61851b24b7e16d55
Vadim Belovalov <vadim.belovalov@gmail.com>
2010-12-07 22:52:11 +0500

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,12 @@ module SessionsHelper
     user == current_user
   end
   
+  def authenticate
+      deny_access unless signed_in?
+    end
+  
   def deny_access
+    store_location
     redirect_to singin_path, :notice => "Please sign in to access this page."
   end
   
